f3d7bda283a73e1034a8ef91c5cd9db174e8df06
Michael Bro <brod.mj@gmail.com>
2018-06-25 14:18:45 +0200

    Evolution of pie chart

--- js/pie-chart.js
+++ js/pie-chart.js
@@ -1,24 +1,51 @@
-
 'use strict';
 document.addEventListener('DOMContentLoaded', function(event) {
-
-    let pie = document.getElementsByClassName("pie-chart");
+ 
+    let pieChart = document.getElementsByClassName("jsPieChart");
     let NS = "http://www.w3.org/2000/svg";
 
-    for (let i = 0; i < pie.length; i++){
-        let p = parseFloat(pie[i].textContent);
-        let svg = document.createElementNS(NS, "svg");
-        let circle = document.createElementNS(NS, "circle");
-        let title = document.createElementNS(NS, "title");
-        circle.setAttribute("r", 16);
-        circle.setAttribute("cx", 16);
-        circle.setAttribute("cy", 16);
-        circle.setAttribute("stroke-dasharray", p + " 100");
-        svg.setAttribute("viewBox", "0 0 32 32");
-        title.textContent = pie[i].textContent;
-        pie[i].textContent = '';
-        svg.appendChild(title);
-        svg.appendChild(circle);
-        pie[i].appendChild(svg);
+    let xhr = new XMLHttpRequest();
+    let responseObject;
+
+    xhr.onload = function(){
+        if(xhr.status === 200){
+
+            responseObject = JSON.parse(xhr.responseText);
+            let skillsObj  = responseObject;
+
+            for (let i = 0; i < skillsObj.skills.length; i++){
+
+                let name = skillsObj.skills[i].name;
+                let progress = parseFloat(skillsObj.skills[i].progress);
+
+                let div = document.createElement("div");
+                let svg = document.createElementNS(NS, "svg");
+                let circle = document.createElementNS(NS, "circle");
+                let title = document.createElementNS(NS, "title");
+
+                div.setAttribute("class", "pie-chart");
+                svg.setAttribute("viewBox", "0 0 32 32");
+                circle.setAttribute("r", 16);
+                circle.setAttribute("cx", 16);
+                circle.setAttribute("cy", 16);
+                circle.setAttribute("stroke-dasharray", progress + " 100");
+                title.textContent = name;
+
+                svg.appendChild(title);
+                svg.appendChild(circle);
+                div.appendChild(svg)
+
+                for(let i = 0; i < pieChart.length; i++){
+                    pieChart[i].appendChild(div);
+                }
+            
+                console.log('name: '+name+' | progress: '+progress);
+
+            }       
+        }
     }
+        
+    xhr.open('GET', '/data/skills.json', true);
+    xhr.send(null);
+
 });//end document ready
